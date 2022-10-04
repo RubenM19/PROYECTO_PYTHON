c9463238a1dd914c2ae3673ac9369b4876ba6bf4
RubenM19 <ruben.mondragon@unmsm.edu.pe>
2022-10-04 14:35:20 -0500

    Terminado Monolitico

--- main_aplication.py
+++ main_aplication.py
@@ -1,149 +1,258 @@
 from typing import List, Dict
-from model.Route import Route
-#enconding: utf-8
+import random
 
-def create_list_route() -> List[Route]:
-    data_routes: List[Dict[str, str | int | float]] =[
-        {
-            "code": "LIM-AYA",
-            "name": "Lima - Ayacucho",
-            "price_base": 55.19,
-            "eco_seat": 8.00,
-            "premium_seat": 16.00,
-            "plane": "A001",
-            "time": "06:30",
-            "min_eco_seats": 120,
-            "max_eco_seats": 130,
-            "min_premium_seats": 10,
-            "max_premium_seats": 20
-        },
-        {
-            "code": "LIM-CUS",
-            "name": "Lima - Cusco",
-            "price_base": 138.51,
-            "eco_seat": 8.00,
-            "premium_seat": 16.00,
-            "plane": "A002",
-            "time": "07:25",
-            "min_eco_seats": 130,
-            "max_eco_seats": 144,
-            "min_premium_seats": 15,
-            "max_premium_seats": 24
-        },
-        {
-            "code": "LIM-ARE",
-            "name": "Lima - Arequipa",
-            "price_base": 90.59,
-            "eco_seat": 8.00,
-            "premium_seat": 16.00,
-            "plane": "A003",
-            "time": "08:10",
-            "min_eco_seats": 115,
-            "max_eco_seats": 138,
-            "min_premium_seats": 16,
-            "max_premium_seats": 22
-        },
-        {
-            "code": "LIM-TAR",
-            "name": "Lima - Tarapoto",
-            "price_base": 71.89,
-            "eco_seat": 8.00,
-            "premium_seat": 16.00,
-            "plane": "A004",
-            "time": "08:50",
-            "min_eco_seats": 100,
-            "max_eco_seats": 120,
-            "min_premium_seats": 12,
-            "max_premium_seats": 18
-        },
-        {
-            "code": "AYA-LIM",
-            "name": "Ayacucho - Lima",
-            "price_base": 40.42,
-            "eco_seat": 7.00,
-            "premium_seat": 16.00,
-            "plane": "A001",
-            "time": "15:45",
-            "min_eco_seats": 100,
-            "max_eco_seats": 115,
-            "min_premium_seats": 10,
-            "max_premium_seats": 15
-        },
-        {
-            "code": "CUS-LIM",
-            "name": "Cusco - Lima",
-            "price_base": 124.32,
-            "eco_seat": 7.00,
-            "premium_seat": 16.00,
-            "plane": "A002",
-            "time": "16:25",
-            "min_eco_seats": 105,
-            "max_eco_seats": 120,
-            "min_premium_seats": 14,
-            "max_premium_seats": 20
-        },
-        {
-            "code": "ARE-LIM",
-            "name": "Arequipa - Lima",
-            "price_base": 86.59,
-            "eco_seat": 7.00,
-            "premium_seat": 16.00,
-            "plane": "A003",
-            "time": "17:15",
-            "min_eco_seats": 100,
-            "max_eco_seats": 110,
-            "min_premium_seats": 13,
-            "max_premium_seats": 18
-        },
-        {
-            "code": "TAR-LIM",
-            "name": "Tarapoto - Lima",
-            "price_base": 86.59,
-            "eco_seat": 7.00,
-            "premium_seat": 16.00,
-            "plane": "A004",
-            "time": "17:50",
-            "min_eco_seats": 90,
-            "max_eco_seats": 105,
-            "min_premium_seats": 10,
-            "max_premium_seats": 15
-        }
-    ]
-    #Lista de objetos aerolinea
-    routes: List[Route] = []
+import statistics
+import operator
 
-    for key, route in enumerate(data_routes):
-        obj_route = Route(str(route['code']), str(route['name']), 
-        float(route['price_base']), int(route['eco_seat']), 
-        int(route['premium_seat']), str(route['plane']), 
-        str(route['time']), int(route['min_eco_seats']),
-        int(route['max_eco_seats']), int(route['min_premium_seats']),
-        int(route['max_premium_seats'])
-        )
-        routes.append(obj_route)
-    
-    return routes
+from config import IGV_TOTAL, CURRENCY_SYMBOL
 
 
 def main():
-    """
-    Función principal del módulo main_plication.py
-    """
-    routes: List[Route] = create_list_route()
 
-    print("\n")
-    print("*"*40)
-    print("REPORTE DE AEROLINEA")
-    print("*"*40)
-    print("\n")
+    # Se inicializa la variable 'flights'
+    flights: List[Dict[str, str | float | int]] = [{
+        'code': 'LIM-AYA',
+        'name': 'Lima - Ayacucho',
+        'base_price': 55.19,
+        'economy_seat': 8,
+        'premium_seat': 16,
+        'airplane': 'A001',
+        'ticket_sale_economy_min_range': 120,
+        'ticket_sale_economy_max_range': 130,
+        'ticket_sale_premium_min_range': 10,
+        'ticket_sale_premium_max_range': 20
+    }, {
+        'code': 'LIM-CUS',
+        'name': 'Lima - Cusco',
+        'base_price': 136.51,
+        'economy_seat': 8,
+        'premium_seat': 16,
+        'airplane': 'A002',
+        'ticket_sale_economy_min_range': 130,
+        'ticket_sale_economy_max_range': 144,
+        'ticket_sale_premium_min_range': 15,
+        'ticket_sale_premium_max_range': 24
+    }, {
+        'code': 'LIM-ARE',
+        'name': 'Lima - Arequipa',
+        'base_price': 90.29,
+        'economy_seat': 8,
+        'premium_seat': 16,
+        'airplane': 'A003',
+        'ticket_sale_economy_min_range': 115,
+        'ticket_sale_economy_max_range': 138,
+        'ticket_sale_premium_min_range': 16,
+        'ticket_sale_premium_max_range': 22
+    }, {
+        'code': 'LIM-TAR',
+        'name': 'Lima - Tarapoto',
+        'base_price': 71.89,
+        'economy_seat': 8,
+        'premium_seat': 16,
+        'airplane': 'A004',
+        'ticket_sale_economy_min_range': 100,
+        'ticket_sale_economy_max_range': 120,
+        'ticket_sale_premium_min_range': 12,
+        'ticket_sale_premium_max_range': 18
+    }, {
+        'code': 'AYA-LIM',
+        'name': 'Ayacucho - Lima',
+        'base_price': 40.42,
+        'economy_seat': 7,
+        'premium_seat': 16,
+        'airplane': 'A001',
+        'ticket_sale_economy_min_range': 100,
+        'ticket_sale_economy_max_range': 115,
+        'ticket_sale_premium_min_range': 10,
+        'ticket_sale_premium_max_range': 15
+    }, {
+        'code': 'CUS-LIM',
+        'name': 'Cusco - Lima',
+        'base_price': 124.32,
+        'economy_seat': 7,
+        'premium_seat': 16,
+        'airplane': 'A002',
+        'ticket_sale_economy_min_range': 105,
+        'ticket_sale_economy_max_range': 120,
+        'ticket_sale_premium_min_range': 14,
+        'ticket_sale_premium_max_range': 20
+    }, {
+        'code': 'ARE-LIM',
+        'name': 'Arequipa - Lima',
+        'base_price': 86.59,
+        'economy_seat': 7,
+        'premium_seat': 16,
+        'airplane': 'A003',
+        'ticket_sale_economy_min_range': 100,
+        'ticket_sale_economy_max_range': 110,
+        'ticket_sale_premium_min_range': 13,
+        'ticket_sale_premium_max_range': 18
+    }, {
+        'code': 'TAR-LIM',
+        'name': 'Tarapoto - Lima',
+        'base_price': 68.42,
+        'economy_seat': 7,
+        'premium_seat': 16,
+        'airplane': 'A004',
+        'ticket_sale_economy_min_range': 90,
+        'ticket_sale_economy_max_range': 105,
+        'ticket_sale_premium_min_range': 10,
+        'ticket_sale_premium_max_range': 15
+    }]
 
-    for key, route in enumerate(routes):
-        print(f"Tienda: {route.name}")
-        print(f"Tienda: {route.code}")
-    
-    print("\n")
-    print("-"*30)
-    print("\n")
+    # Se inicializa la variable que almacenará el total de tickets
+    total_ticket: int = 0
+
+    # Se inicializa la variable que almacenará la venta total de los pasajes económicos
+    total_economic_tickets_sales: float = 0
+
+    # Se inicializa la variable que almacenará la venta total de los pasajes premium
+    total_premium_tickets_sales: float = 0
+
+    # Se inicializa la variable que almacenará el total de IGV
+    total_IGV: float = 0
+
+    # Se inicializa la variable all_cheap_tickets que almacenará los precios de pasaje económico
+    all_cheap_tickets: List[float] = []
+
+    # Se inicializa la variable all_premium_tickets que almacenará los precios de pasaje premium
+    all_premium_tickets: List[float] = []
+
+    # Se inicializa la variable all_flights que almacenará los  vuelos programados
+    all_scheduled_flights: List[Dict[str, str | float]] = []
+
+    # Iteración para cada vuelo
+    for key, flight in enumerate(flights):
+
+        # Se inicializa la variable que almacenará el total de pasajes económicos vendidos
+        economic_ticket: int = random.randint(int(flight['ticket_sale_economy_min_range']), int(
+            flight['ticket_sale_economy_max_range']))
+
+        # Se inicializa la variable que almacenará el total de pasajes premium vendidos
+        premium_ticket: int = random.randint(int(flight['ticket_sale_premium_min_range']), int(
+            flight['ticket_sale_premium_max_range']))
+
+        # Sumará los pasajes vendidos
+        total_ticket += economic_ticket + premium_ticket
+
+        # Iteración por cada asiento económico de cada vuelo
+        for i in range(economic_ticket):
+
+            # Se inicializa el precio del pasaje económico sin IGV
+            economic_ticket_cost: float = (
+                float(flight['base_price']) + float(flight['economy_seat']))
+
+            # Se agrega a la lista all_cheap_tickets el precio de un pasaje con asiento económico
+            all_cheap_tickets.append(economic_ticket_cost)
+
+            # Se inicializa el precio total del pasaje económico con IGV
+            total_economic_ticket_cost: float = economic_ticket_cost + \
+                economic_ticket_cost * (IGV_TOTAL / 100)
+
+            # Se va sumando el impuesto de cada pasaje económico del vuelo
+            total_IGV += economic_ticket_cost * (IGV_TOTAL / 100)
+
+            # Se va sumando el precio total del pasaje económico del vuelo
+            total_economic_tickets_sales += total_economic_ticket_cost
+
+        # Iteración por cada asiento premium de cada vuelo
+        for i in range(premium_ticket):
+
+            # Se inicializa el precio del pasaje premium sin IGV
+            premium_ticket_cost: float = (
+                float(flight['base_price']) + float(flight['premium_seat']))
+
+            # Se agrega a la lista all_premium_tickets el precio de un pasaje con asiento premium
+            all_premium_tickets.append(premium_ticket_cost)
+
+            # Se inicializa el precio total del pasaje premium con IGV
+            total_premium_ticket_cost: float = premium_ticket_cost + \
+                premium_ticket_cost * (IGV_TOTAL / 100)
+
+            # Se va sumando el impuesto de cada pasaje premium del vuelo
+            total_IGV += premium_ticket_cost * (IGV_TOTAL / 100)
+
+            # Se va sumando el precio total del pasaje premium del vuelo
+            total_premium_tickets_sales += total_premium_ticket_cost
+
+        # Sumando total de tickets premium y total de tickets económicos vendidos y
+        total_ticket_sales = round(
+            total_premium_tickets_sales + total_economic_tickets_sales, 2)
+
+        total_tickets_per_fly: int = economic_ticket + premium_ticket
+        # Creamos el diccionario para los vuelos programados
+        scheduled_flights: Dict[str, str | int | float] = {
+            "route": flight["name"],
+            "airplane": flight["airplane"],
+            "total_tickets_sales": total_ticket_sales,
+            "total_tickets_per_fly": total_tickets_per_fly,
+
+        }
+        all_scheduled_flights.append(scheduled_flights)
+
+    # Se imprime la cantidad de pasajes por todos los vuelos
+    # Pregunta 1
+    print(f"Se vendieron un total de {total_ticket} tickets")
+
+    print("-" * 30)
+
+    # Se imprime el total de pasajes económicos que se han vendido
+    # Pregunta 2
+    print(f"Se ingresó un total de $ {round(total_economic_tickets_sales,2)} en pasajes económicos")
+
+    # Se imprime el total de pasajes premium que se han vendido
+    # Pregunta 3
+    print(f"Se ingresó un total de $ {round(total_premium_tickets_sales,2)} en pasajes premium")
+
+    # Se imprime el total IGV que se ha cobrado
+    # Pregunta 4
+    print(f"Se cobró un total de $ {round(total_IGV,2)} en IGV")
+    print("-" * 30)
+
+    # Pregunta 5
+    # Se imprime el valor promedio del pasaje económico
+    average_economic_ticket_cost: float = statistics.mean(all_cheap_tickets)
+    average_formatted_economy_ticket: str = "{}{:,.2f}".format(
+        CURRENCY_SYMBOL, average_economic_ticket_cost)
+    print(f"El valor promedio del pasaje económico es: {average_formatted_economy_ticket}")
+    print("-" * 30)
+
+    # Pregunta 6
+    # Se imprime el valor promedio del pasaje premium
+    average_premium_ticket_cost: float = statistics.mean(all_premium_tickets)
+    average_formatted_premium_ticket: str = "{}{:,.2f}".format(
+        CURRENCY_SYMBOL, average_premium_ticket_cost)
+    print(f"El valor promedio del pasaje premium es: {average_formatted_premium_ticket}")
+    print("-" * 30)
+
+    # Ordenemos lista de vuelos según el número de pasajeros (de menor a mayor)
+    sorted_passengers: List[Dict[str, str | float]] = sorted(
+        all_scheduled_flights, key=lambda x: x["total_tickets_per_fly"])
+    print("-" * 30)
+
+    # Pregunta 7
+    # Se obtiene el maximo de pasajeros a traves de los pasajes vendidos
+    print(
+        f"El vuelo con la mayor cantidad de pasajeros es: {sorted_passengers[-1]['route']} con una cantidad de {sorted_passengers[-1]['total_tickets_per_fly']}")
+
+    # Pregunta 8
+    # Se obtiene el mínimo de pasajeros a traves de los pasajes vendidos
+    print(f"El vuelo con la menor cantidad de pasajeros es: {sorted_passengers[0]['route']} con una cantidad de {sorted_passengers[0]['total_tickets_per_fly']}")
+    print("-" * 30)
+
+    # Pregunta 9
+    # Ordenemos lista de vuelos según los ingresos por la venta de asientos (de mayor a menor)
+    order_total_tickets_sales: List[Dict[str, str | float]] = sorted(
+        all_scheduled_flights, key=lambda x: x["total_tickets_sales"], reverse=True)
+
+    # Iteramos los 3 veces la lista ordeana de vuelos
+    for i in range(3):
+        print(f"El {i+1}° vuelo con mayor ingresos es: {order_total_tickets_sales[i]['route']} con un total de {CURRENCY_SYMBOL}{order_total_tickets_sales[i]['total_tickets_sales']}")
+
+    print("-" * 50)
+    # Pregunta 10
+    print(f"El avión que transportó mas pasajeros es: {sorted_passengers[-1]['airplane']}")
+    print("-" * 30)
 
 if __name__ == "__main__":
     main()
